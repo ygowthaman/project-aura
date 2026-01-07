9468b45e1c9b80ebec94e48b2425dea3237ab168
Y Gowthaman <ygowthaman@outlook.com>
2026-01-07 11:22:53 -0800

    Test commit from new system

--- src/main/java/com/winterflame/aura/controller/DispositionRestController.java
+++ src/main/java/com/winterflame/aura/controller/DispositionRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DispositionRestController {
 
-    @GetMapping("/disposition")
+    @GetMapping("/dispositions")
     public String getAllDisposition() {
         return "All Disposition";
     }
